eeed79bce24bf328a1b11c8c94f02377668af8ce
Heeyeon Kim <go1927hm@gmail.com>
2025-01-25 21:42:26 +0900

    [ADD] Base Components

---
+++ survey-pie/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+import ProgressIndicator from './components/ProgressIndicator';
+import QuestionBox from './components/QuestionBox';
+
+function App() {
+  const questions = [
+    {
+      title: '질문1입니다.',
+      desc: '설명1입니다.',
+      type: 'text',
+      required: false,
+      options: [],
+    },
+    {
+      title: '질문2입니다.',
+      desc: '설명2입니다.',
+      type: 'text',
+      required: false,
+      options: [],
+    },
+  ];
+
+  const step = 0;
+
+  const [answers, setAnswers] = useState([]);
+  return (
+    <div className="App">
+      <ProgressIndicator />
+      <QuestionBox
+        question={questions[step]}
+        step={step}
+        quetionsLength={questions.length}
+        answer={answers[step]}
+        setAnswer={(newAnswer) => {
+          setAnswers((answers) => {
+            const newAnswers = [...answers];
+            newAnswers[step] = newAnswer;
+            return newAnswers;
+          });
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ survey-pie/src/components/Body/index.js
@@ -0,0 +1,14 @@
+import TextInput from '../TextInput';
+export default function Body({ type, answer, setAnswer }) {
+  let InputComponent = null;
+  if (type === 'select') {
+  } else if (type === 'text') {
+    InputComponent = TextInput;
+  } else if (type === 'textArea') {
+  }
+  return (
+    <>
+      <InputComponent answer={answer} setAnswer={setAnswer} />
+    </>
+  );
+}
